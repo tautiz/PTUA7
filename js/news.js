dda742fffb45395c01b046ce7008c51cb6b77bfa
Tautvydas Dulskis <tautvydas.dulskis@venipak.com>
2023-09-27 21:01:19 +0300

    Chuck noris Code with Fetch, createElement and LocalStorage features

--- js/news.js
+++ js/news.js
@@ -1,30 +1,69 @@
-const data = document.getElementById('data');
-const saveBtn = document.getElementById('save');
-const displayBtn = document.getElementById('display');
-const storageTypeBtn = document.getElementById('storage');
-let storage = localStorage;
-let storageType = true;
+const chuckCategories = document.getElementById('chuckCategories');
+const chuckJokes = document.getElementById('chuckJokes');
+const favBtn = document.getElementById('megstami');
+const baseUrl = 'https://api.chucknorris.io/jokes';
+let listOfJokes = [];
 
-saveBtn.onclick = function () {
-    storage.setItem('raktaZodis', data.value);
-    // Cia informuojam vartotoja apie sekminga ssaugojima
-    // ...
+// 1. Parsisiunciame "Kategoriju" sarasa.
+// https://api.chucknorris.io/jokes/categories
+fetch(baseUrl + '/categories')
+    .then(response => response.json())
+    .then(result => renderCategories(result))
+    .catch(err => console.error(err));
+
+// 2. Generuojame naujus elementus pagal gautas kategorijas
+function renderCategories(list) {
+    list.forEach(item => generateCategoryItem(item));
 }
 
-displayBtn.onclick = function () {
-    const newElementas = document.createElement('div');
-    newElementas.textContent = storage.getItem('raktaZodis');
-    document.querySelector('main').append(newElementas);
+// 3. Kiekvienam elementui prisegame 'click' eventa, kad kai ji
+//    paspausime mums sius nauja uzklausa gauti juokeli pagal
+//    pasirinkta kategorija
+
+// https://api.chucknorris.io/jokes/random?category={category}
+function generateCategoryItem(item) {
+    const li = document.createElement('li');
+    li.textContent = item;
+    li.onclick = generateJoke;
+    chuckCategories.append(li);
 }
 
-storageTypeBtn.onclick = function () {
-    storageType = !storageType
-    if (!storageType) {
-        storage = sessionStorage;
-        storageTypeBtn.textContent = 'Session'
-        return;
-    }
-    storageTypeBtn.textContent = 'Local'
-    storage = localStorage;
+function generateJoke(e) {
+    const categoryName = e.target.textContent;
+    fetch(baseUrl + '/random?category=' + categoryName)
+        .then(response => response.json())
+        .then(joke => generateJokeElement(joke))
+        .catch(err => console.error(err));
 }
 
+// 4. Kai gaunamas "Juokelis" sukuriamas <LI> elementas kuriame bus
+//    "Juokelio tekstas" ir mygtukas "☆" "★" (saugoti i LocalStorage)
+function generateJokeElement(joke) {
+    const li = document.createElement('li');
+    const starBtn = document.createElement('span');
+    starBtn.textContent = '☆';
+    starBtn.dataset.juokelis = joke.value;
+    starBtn.onclick = saveToFavorites;
+    li.textContent = joke.value;
+    li.prepend(starBtn);
+    chuckJokes.append(li);
+}
+
+function saveToFavorites(e) {
+    const zvaigzdMygt = e.target;
+    const juokelisDataSete = zvaigzdMygt.dataset.juokelis;
+    listOfJokes.push({'value': juokelisDataSete});
+    localStorage.setItem('juokeliai', JSON.stringify(listOfJokes));
+    zvaigzdMygt.textContent = '★';
+}
+
+// 5. Atidarius svetaine is naujo ir paspaudus mygtuka "Megstami",
+//    atvaizduoti vius megstamus (paspausti su ★) elementus
+//    is LocalStorage
+favBtn.onclick = displayFavoritesJokes;
+
+function displayFavoritesJokes() {
+    const stingList = localStorage.getItem('juokeliai');
+    const jokeList = JSON.parse(stingList);
+    jokeList.forEach(joke => generateJokeElement(joke));
+}
